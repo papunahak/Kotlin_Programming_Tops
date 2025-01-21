8e698011cac126d4eb7441d9b4927d30fcf425c1
papunahak <115139623+papunahak@users.noreply.github.com>
2025-01-21 15:58:29 +0530

    Update TASK.kt

--- TASK.kt
+++ TASK.kt
@@ -1,49 +1,12 @@
-package Collection
-
-
-open class Person(val name:String, val age:String)
-
-class Students(name:String, age:String, val grade:Int):Person(name,age){
-fun printDetails(){
-    println("Student name:$name, age:$age, grade:$grade")
-}
-}
-
-class Teacher(name:String,age:String,val Subject:String):Person(name, age){
-    fun printDetails(){
-        println("Teacher name:$name, age:$age, subject:$Subject")
-    }
-}
-
-object School{
-   private var totalPeople: Int = 0
-
-    fun addPerson(){
-        totalPeople++
-    }
-    fun getTotalPeople():Int{
-        return totalPeople
-    }
-}
-
-
+package assignment
 
 fun main() {
-
-    var s1= Students("PAPU NAHAK", "24", 10)
-    var s2=Students("SMIT", "23", 9)
-
-    var t1=Teacher("Prakruti ma'am", "27", "Kotlin")
-
-    School.addPerson()
-    School.addPerson()
-    School.addPerson()
-
-    s1.printDetails()
-    s2.printDetails()
-
-    t1.printDetails()
-
-    println("total people in school ${School.getTotalPeople()}")
+    var sum=0
+    for(i in 1..100){
+        if(i%2==0){
+            sum+=i
+        }
+    }
+    println("the sum of all  even numbeer is 1 to 100:  $sum")
 
 }
